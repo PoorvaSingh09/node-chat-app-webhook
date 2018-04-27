7945e9758e27a0512f0c7ae1c310a9a9e02b2a5b
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-27 17:09:01 +0530

    Send response from route

--- server.js
+++ server.js
@@ -7,23 +7,24 @@ var port = process.env.PORT || 3000;
 app.get('/repos/:owner/:repo/hooks', (req, res) => {
   console.log(req);
   console.log(res);
+  res.send({url:'GET /repos/:owner/:repo/hooks', status: true});
 });
 
 app.post('/repos/:owner/:repo/hooks', (req, res) => {
   console.log(req);
-  console.log(res);
+  res.send({url:'POST /repos/:owner/:repo/hooks', status: true});
 });
 
 app.post('/', (req, res) => {
   console.log(req);
   console.log(res);
-  return {status: true};
+  res.send({url:'POST /', status: true});
 });
 
 app.get('/', (req, res) => {
   console.log(req);
   console.log(res);
-  return {status: true};
+  res.send({url:'GET /', status: true});
 });
 
 app.listen(port, () => {
