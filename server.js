f25c1767ed3a30d41965d0dc9c4c7806eb996c07
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-27 16:03:59 +0530

    Add GET /repos route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.get('/repos/:owner/:repo/hooks', (req, res) => {
+  console.log(res);
+});
+
 app.listen(port, () => {
   console.log(`Server is up at ${port}`);
 });
